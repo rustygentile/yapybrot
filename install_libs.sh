321a4e1638e664917e82f7e354e6c3236f77e4b5
Rusty Gentile <18237687+rustygentile@users.noreply.github.com>
2022-12-08 14:02:52 -0500

    wip - specifying python executable and include path for cmake in manylinux workflow

--- install_libs.sh
+++ install_libs.sh
@@ -2,7 +2,10 @@ libs="pybind11 xtl xtensor xtensor-python"
 for lib in $libs
 do
 	mkdir lib/$lib/build && cd $_
-	cmake -DCMAKE_INSTALL_PREFIX=../../../bin/ ..
+	cmake -DPYTHON_EXECUTABLE:FILEPATH=/opt/python/cp310-cp310/bin/python \
+	-DPYTHON_EXECUTABLE:FILEPATH=/opt/python/cp310-cp310/bin/python \
+	-DPYTHON_INCLUDE_DIR:PATH=/opt/python/cp310-cp310/include/python3.10 \
+	-DCMAKE_INSTALL_PREFIX=../../../bin/ ..
 	make install
 	cd ../../..
 	rm -r lib/$lib/build
