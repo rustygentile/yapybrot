423c04735bfbbb6feabc30ebdb65883be0e477ce
Rusty Gentile <18237687+rustygentile@users.noreply.github.com>
2022-12-08 12:04:23 -0500

    wip - removed PYPATH from install_libs

--- install_libs.sh
+++ install_libs.sh
@@ -1,13 +1,10 @@
 pip install -r requirements.txt
 
-PYPATH=$(which python)
-
 libs="pybind11 xtl xtensor xtensor-python"  
 for lib in $libs
 do
-	mkdir lib/$lib/build
-	cd lib/$lib/build
-	cmake -DCMAKE_INSTALL_PREFIX=../../../bin/ -DPYTHON_EXECUTABLE:FILEPATH=$PYPATH ..
+	mkdir lib/$lib/build && cd $_
+	cmake -DCMAKE_INSTALL_PREFIX=../../../bin/ ..
 	make install
 	cd ../../..
 	rm -r lib/$lib/build
