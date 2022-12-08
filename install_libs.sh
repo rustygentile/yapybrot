811015e8f3322c139b07b1350ade6c495e57fec0
Rusty Gentile <18237687+rustygentile@users.noreply.github.com>
2022-12-08 17:09:05 -0500

    wip - setup manylinux to work for all python versions, added manylinux test with artifact upload

--- install_libs.sh
+++ install_libs.sh
@@ -1,12 +1,20 @@
-libs="pybind11 xtl xtensor xtensor-python"  
-for lib in $libs
+CPP_LIBS="pybind11 xtl xtensor xtensor-python"  
+
+for PY_VER in "cp38-cp38 3.8" "cp39-cp39 3.9" "cp310-cp310 3.10"
 do
-	mkdir lib/$lib/build && cd $_
-	cmake -DPYTHON_EXECUTABLE:FILEPATH=/opt/python/cp310-cp310/bin/python \
-	-DPYTHON_EXECUTABLE:FILEPATH=/opt/python/cp310-cp310/bin/python \
-	-DPYTHON_INCLUDE_DIR:PATH=/opt/python/cp310-cp310/include/python3.10 \
-	-DCMAKE_INSTALL_PREFIX=../../../bin/ ..
-	make install
-	cd ../../..
-	rm -r lib/$lib/build
+	# Install python libraries
+	/opt/python/${PY_VER[0]}/bin/python -m pip install -r requirements.txt
+	
+	# Build the C++ libraries
+	for CPP_LIB in $CPP_LIBS
+	do
+		mkdir lib/$CPP_LIB/build && cd $_
+		cmake \
+			-DPYTHON_EXECUTABLE:FILEPATH=/opt/python/${PY_VER[0]}/bin/python \
+			-DPYTHON_INCLUDE_DIR:PATH=/opt/python/${PY_VER[0]}/include/python${PY_VER[1]} \
+			-DCMAKE_INSTALL_PREFIX=../../../bin/ ..
+		make install
+		cd ../../..
+		rm -r lib/$CPP_LIB/build
+	done
 done
